fd98b422f91d90a99c1fdb51a6a635ba83fc9c89
Quinn Weber <quinn@convoy.com>
2020-07-09 16:06:23 -0700

    add file upload

---
+++ components/lib/exif.ts
@@ -0,0 +1,29 @@
+import EXIF from "exif-js";
+
+export async function getLatLong(
+  file: File
+): Promise<{ latitude: number; longitude: number }> {
+  return new Promise((resolve, _reject) => {
+    EXIF.getData(file as any, function () {
+      const exifData = this.exifdata;
+      const latitudeComponents: number[] = exifData.GPSLatitude;
+      const latitudeRef: "N" | "S" = exifData.GPSLatitudeRef;
+      const longitudeComponents: number[] = exifData.GPSLongitude;
+      const longitudeRef: "E" | "W" = exifData.GPSLongitudeRef;
+
+      const latitude =
+        (latitudeRef === "N" ? 1 : -1) *
+        latitudeComponents.reduce((total, value, index) => {
+          return total + value / Math.pow(60, index);
+        }, 0);
+
+      const longitude =
+        (longitudeRef === "E" ? 1 : -1) *
+        longitudeComponents.reduce((total, value, index) => {
+          return total + value / Math.pow(60, index);
+        }, 0);
+
+      resolve({ latitude, longitude });
+    });
+  });
+}
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,32 +1,57 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import fetch from "isomorphic-unfetch";
 
 import Layout from "../components/Layout";
+import { getLatLong } from "../components/lib/exif";
 
 const DynamicComponentWithNoSSR = dynamic(() => import("../components/Map"), {
   ssr: false,
 });
 
 export default () => {
-  const [stateName, setStateName] = useState("");
-  const [finalStateName, setFinalStateName] = useState("");
+  const [typedStateName, setTypedStateName] = useState("");
+  const [finalStateNames, setFinalStateNames] = useState([""]);
   return (
     <Layout>
       <input
         type="text"
-        value={stateName}
-        onChange={(event) => setStateName(event.target.value)}
+        value={typedStateName}
+        onChange={(event) => setTypedStateName(event.target.value)}
       />
       <input
         type="submit"
         value="Submit"
         onClick={async (event) => {
-          setFinalStateName(stateName);
+          setFinalStateNames([...finalStateNames, typedStateName]);
           event.preventDefault();
         }}
       />
+      <input
+        type="file"
+        onChange={async (event) => {
+          if (event.target.files) {
+            const coordinates = await getLatLong(event.target.files?.[0]);
+            const result = await fetch(
+              `https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinates.longitude},${coordinates.latitude}.json?access_token=pk.eyJ1IjoicXN3ZWJlciIsImEiOiJjam5nZTZuazgwMTlkM2twYXpjYmpqeTBjIn0.viU-jQrjmOf40aONFwjQdQ`
+            );
+            const data = await result.json();
+            for (let feature of data.features) {
+              if (feature.context) {
+                for (let context of feature.context) {
+                  if ((context.id as string).startsWith("region")) {
+                    const uploadedState = context.text as string;
+                    setFinalStateNames([...finalStateNames, uploadedState]);
+                    break;
+                  }
+                }
+              }
+            }
+          }
+        }}
+      />
       <DynamicComponentWithNoSSR
-        stateNames={["Alaska", "Texas", finalStateName]}
+        stateNames={["Alaska", "Texas", ...finalStateNames]}
       />
     </Layout>
   );
