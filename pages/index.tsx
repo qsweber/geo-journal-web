0a40196280651f95fbf097c82f92d1d05f608448
Quinn Weber <quinn@quinnweber.com>
2020-09-03 23:23:18 -0700

    exif works!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 
 import Layout from "../components/Layout";
 import { Image } from "../components/Map";
-import { getLatLong } from "../lib/exif";
 import MapboxClient from "../clients/mapbox";
 import { CognitoClient } from "../clients/cognito";
-import { ApiClient, Image as ApiImage, ImageMetadata } from "../clients/api";
+import { ApiClient, Image as ApiImage } from "../clients/api";
 import { User } from "../interfaces";
+import ImageUpload from "../components/ImageUpload";
 
 const DynamicComponentWithNoSSR = dynamic(() => import("../components/Map"), {
   ssr: false,
@@ -28,89 +28,56 @@ async function getInitialData(): Promise<{
   }
   const api = new ApiClient(user);
   const imageMetadata = await api.getImages();
-  const images = await Promise.all(imageMetadata.map(getImageFromApiImage))
+  const images = await Promise.all(imageMetadata.map(getImageFromApiImage));
   return { api, user, savedImages: images };
 }
 
 async function getImageFromApiImage(apiImage: ApiImage): Promise<Image> {
-  const imgBlob = await (await fetch(apiImage.presignedUrl, { method: 'GET'})).blob();
-  console.log('hi', imgBlob);
+  const imgBlob = await (
+    await fetch(apiImage.presignedUrl, { method: "GET" })
+  ).blob();
+  console.log("hi", imgBlob);
   const imgSrc = URL.createObjectURL(imgBlob);
-  console.log('hi2');
+  console.log("hi2");
   return {
-    stateName: await mapbox.getStateFromCoordinates(
-      { latitude: apiImage.metadata.latitude, longitude: apiImage.metadata.longitude}
-    ),
+    stateName: await mapbox.getStateFromCoordinates({
+      latitude: apiImage.metadata.latitude,
+      longitude: apiImage.metadata.longitude,
+    }),
     latitude: apiImage.metadata.latitude,
     longitude: apiImage.metadata.longitude,
     takenAt: apiImage.metadata.takenAt,
     imgSrc,
-  }
-}
-
-async function getImageFromUploadedFile(file: File): Promise<Image> {
-  const coordinates = await getLatLong(file);
-  return {
-    stateName: await mapbox.getStateFromCoordinates(
-      coordinates
-    ),
-    ...coordinates,
-    takenAt: new Date(file.lastModified),
-    imgSrc: URL.createObjectURL(file),
-  }
-}
-
-async function saveImage(file: File, image: Image, api: ApiClient): Promise<void> {
-  const imageMetadata: ImageMetadata = {
-    latitude: image.latitude,
-    longitude: image.longitude,
-    takenAt: new Date(file.lastModified),
-    name: file.name,
-  }
-
-  const presignStuff = await api.presign(imageMetadata);
-  const formData = new FormData();
-  Object.keys(presignStuff.data).forEach((k) => {
-    formData.append(k, presignStuff.data[k]);
-  });
-  formData.append("file", file);
-  await fetch(presignStuff.url, {
-    method: "POST",
-    body: formData,
-  });
+  };
 }
 
 const IndexPage = () => {
   const [images, setImages] = useState<Image[]>([]);
   const result = useSWR("user", getInitialData);
 
+  const states = images
+    .map((i) => i.stateName)
+    .concat(result.data ? result.data.savedImages.map((i) => i.stateName) : []);
+  console.log(states);
+
   return (
     <Layout loggedInUser={result.data?.user}>
       <div style={{ paddingBottom: 8 }}>
-        <input
-          type="file"
-          multiple
-          onChange={async (event) => {
-            if (event.target.files) {
-              const fileList = event.target.files;
-              const newImages: Image[] = [];
-              for (let i = 0; i < fileList.length; i++) {
-                const file = fileList.item(i);
-                if (file) {
-                  const image = await getImageFromUploadedFile(file);
-                  newImages.push(image);
-                  if (result.data) {
-                    await saveImage(file, image, result.data.api);
-                  }
-                }
-              }
-              setImages([...images, ...newImages]);
-            }
+        <ImageUpload
+          loggedInUser={result.data?.user}
+          api={result.data?.api}
+          mapbox={mapbox}
+          callback={(newImages: Image[]) => {
+            setImages([...images, ...newImages]);
           }}
         />
-        <span>{`States you've been to: ${new Set(images.concat(result.data ? result.data.savedImages : [])).size} / 50`}</span>
+        <span>{`States you've been to: ${
+          new Set(states.filter((s) => !!s)).size
+        } / 50`}</span>
       </div>
-      <DynamicComponentWithNoSSR images={images.concat(result.data ? result.data.savedImages : [])} />
+      <DynamicComponentWithNoSSR
+        images={images.concat(result.data ? result.data.savedImages : [])}
+      />
     </Layout>
   );
 };
